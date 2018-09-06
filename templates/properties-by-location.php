d2267e43e9e198679e5152b5add91d9eda133a46
git-palace <edeveloper4@gmail.com>
2018-09-06 00:39:21 -0700

    updates

--- templates/properties-by-location.php
+++ templates/properties-by-location.php
@@ -8,62 +8,72 @@ $properties = SI()->getPropertiesByCity( get_the_title() );
 <section class="searched-results">
 	<div class="container-fluid">
 
-		<div class="row">
+		<?php if( count( $properties ) ) : ?>
+		
+			<div class="row">
 
-			<div class="col-xs-6 h-850px">
-				<div class="row">
-					<?php foreach ( $properties as $idx => $property ) : ?>
-			    	<?php $pictures = json_decode( $property->pictures ); ?>
-						
-						<div class="col-xs-6 matched-query">
+				<div class="col-xs-6 h-850px">
+					<div class="row">
+						<?php foreach ( $properties as $idx => $property ) : ?>
+						<?php $pictures = json_decode( $property->pictures ); ?>
 							
-							<?php if ( is_array( $pictures ) && !empty( $pictures )  ): ?>
+							<div class="col-xs-6 matched-query">
+								
+								<?php if ( is_array( $pictures ) && !empty( $pictures )  ): ?>
 
-								<div id="listing-images-<?php _e( $idx )?>" class="carousel slide" data-ride="carousel" data-interval="false">
-							    <!-- Wrapper for slides -->
-							    <div class="carousel-inner">
-							    	
-							    	<?php foreach ( $pictures as $p_idx => $picture ): ?>
-							      	<div class="item <?php _e( $p_idx == 0 ? 'active' : '' ) ?>">
-								        <img src="<?php _e( $picture->url ); ?>" style="width:100%;">
-								      </div>
-							    	<?php endforeach; ?>
-							      
-							    </div>
+									<div id="listing-images-<?php _e( $idx )?>" class="carousel slide" data-ride="carousel" data-interval="false">
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner">
+										
+										<?php foreach ( $pictures as $p_idx => $picture ): ?>
+										<div class="item <?php _e( $p_idx == 0 ? 'active' : '' ) ?>">
+											<img src="<?php _e( $picture->url ); ?>" style="width:100%;">
+										</div>
+										<?php endforeach; ?>
+									
+									</div>
 
-							    <!-- Left and right controls -->
-							    <a class="left carousel-control" role="button" data-target="#listing-images-<?php _e( $idx )?>" data-slide="prev">
-							      <span><i class="fa fa-chevron-left"></i></span>
-							    </a>
-							    <a class="right carousel-control" role="button" data-target="#listing-images-<?php _e( $idx )?>" data-slide="next">
-							      <span><i class="fa fa-chevron-right"></i></span>
-							    </a>
-							  </div>
-							<?php else: ?>
-								<div class="no-image"></div>
-
-							<?php endif;?>
-
-							<div class="summary">
-								<p><a target="_blank" href="/single-property/<?php _e( getValidatedValue( $property, 'listingID' ) ); ?>"><?php _e( getValidatedValue( $property, 'address' ) ); ?>, <?php _e( getValidatedValue( $property, 'city' ) ); ?></a></p>
-								<p>
-									<a target="_blank" href="/single-property/<?php _e( getValidatedValue( $property, 'listingID' ) ); ?>">
-										<?php _e( getValidatedValue( $property, 'beds_num', 'No' ) ); ?> Bed(s)&nbsp;∙&nbsp;
-										<?php _e( getValidatedValue( $property, 'baths_num', 'No' ) ); ?> Bath(s)
+									<!-- Left and right controls -->
+									<a class="left carousel-control" role="button" data-target="#listing-images-<?php _e( $idx )?>" data-slide="prev">
+									<span><i class="fa fa-chevron-left"></i></span>
 									</a>
-								</p>
-								<p><a target="_blank" href="/single-property/<?php _e( getValidatedValue( $property, 'listingID' ) ); ?>"><?php _e( '$' . number_format( floatval( getValidatedValue( $property, 'list_price', 0 ) ) ) ); ?></a></p>
+									<a class="right carousel-control" role="button" data-target="#listing-images-<?php _e( $idx )?>" data-slide="next">
+									<span><i class="fa fa-chevron-right"></i></span>
+									</a>
+								</div>
+								<?php else: ?>
+									<div class="no-image"></div>
+
+								<?php endif;?>
+
+								<div class="summary">
+									<p><a target="_blank" href="/single-property/<?php _e( getValidatedValue( $property, 'listingID' ) ); ?>"><?php _e( getValidatedValue( $property, 'address' ) ); ?>, <?php _e( getValidatedValue( $property, 'city' ) ); ?></a></p>
+									<p>
+										<a target="_blank" href="/single-property/<?php _e( getValidatedValue( $property, 'listingID' ) ); ?>">
+											<?php _e( getValidatedValue( $property, 'beds_num', 'No' ) ); ?> Bed(s)&nbsp;∙&nbsp;
+											<?php _e( getValidatedValue( $property, 'baths_num', 'No' ) ); ?> Bath(s)
+										</a>
+									</p>
+									<p><a target="_blank" href="/single-property/<?php _e( getValidatedValue( $property, 'listingID' ) ); ?>"><?php _e( '$' . number_format( floatval( getValidatedValue( $property, 'list_price', 0 ) ) ) ); ?></a></p>
+								</div>
 							</div>
-						</div>
-					<?php endforeach; ?>
+						<?php endforeach; ?>
+					</div>
+				</div>
+
+				<div class="col-xs-6 h-850px search-result-map-container">
+					<div id="search-result-map"></div>
+				</div>
+
+			</div>
+		
+		<?php else: ?>
+			<div class="row">
+				<div class="col-xs-12">
+				<h1 class="text-center" style="margin: 250px 0px;">No result in this city.</h1>
 				</div>
 			</div>
-
-			<div class="col-xs-6 h-850px search-result-map-container">
-				<div id="search-result-map"></div>
-			</div>
-
-		</div>
+		<?php endif; ?>
 
 	</div>
 </section>
